518085d31b0bcc6609c078f229a25d06ba000cec
Justin Davis <justn.davis@gmail.com>
2019-10-13 18:49:50 -0400

    updated filepath for *nix

--- names_scores.py
+++ names_scores.py
@@ -1,19 +1,19 @@
 # Problem 22
 #
-# Using names.txt, a 46K text file containing over five-thousand first names, 
-# begin by sorting it into alphabetical order. Then owrking out the 
-# alphabetical value for each name, multiply this value by its alphabetical 
+# Using names.txt, a 46K text file containing over five-thousand first names,
+# begin by sorting it into alphabetical order. Then owrking out the
+# alphabetical value for each name, multiply this value by its alphabetical
 # position in the list to obtain a name score.
 #
-# For example, when the list is sorted into alphabetical order, COLIN, which is 
-# worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So COLIN 
+# For example, when the list is sorted into alphabetical order, COLIN, which is
+# worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So COLIN
 # would obtain a score of 938 x 53 - 49714.
 #
 # What is the total of all the name scores in the file?
 
 import csv
 import string
-filename = "text files\p022_names.txt"
+filename = "text files/p022_names.txt"
 
 with open(filename) as file_object:
     reader = csv.reader(file_object)
@@ -25,7 +25,7 @@ names = names.pop()
 
 names.sort()
 
-my_abc = { string.ascii_uppercase[char] : char+1 
+my_abc = { string.ascii_uppercase[char] : char+1
     for char in range(len(string.ascii_uppercase))}
 
 answer = 0
