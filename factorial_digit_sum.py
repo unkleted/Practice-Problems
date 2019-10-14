fd0a517ac16b23e79dfa784cdbae1d9ac84130c8
unkleted <jdavis@tjctechnology.com>
2019-10-14 10:06:36 -0400

    Problem 20

---
+++ factorial_digit_sum.py
@@ -0,0 +1,18 @@
+# Problem 20
+#
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
+from math_stuff import factorial
+
+fact_100 = factorial(100)
+my_sum = 0
+
+for d in str(fact_100):
+    my_sum += int(d)
+
+print(my_sum)
