a688b27e1ffef880ddb36c4f04cf28bfa24235a1
Justin Davis <justn.davis@gmail.com>
2019-10-14 23:03:25 -0400

    prob 26

---
+++ reciprocal_cycles.py
@@ -0,0 +1,36 @@
+# Problem 26
+#
+# A unit fraction contains 1 in the numerator. The decimal representation of 
+# the unit fractions with denominators 2 to 10 are given:
+# 1/2	= 	0.5
+# 1/3	= 	0.(3)
+# 1/4	= 	0.25
+# 1/5	= 	0.2
+# 1/6	= 	0.1(6)
+# 1/7	= 	0.(142857)
+# 1/8	= 	0.125
+# 1/9	= 	0.(1)
+# 1/10	= 	0.1
+# Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be 
+# seen that 1/7 has a 6-digit recurring cycle.
+#
+# Find the value of d < 1000 for which 1/d contains the longest recurring cycle 
+# in its decimal fraction part.
+
+from math_stuff import prime_factors
+
+denom = 1
+max_cycle = 0
+
+for d in range(2,1000):
+    pf = prime_factors(d)
+    for f in pf:
+        if f != 2 and f != 5:
+            period = 1
+            while 10**period % f != 1:
+                period +=1
+            if period > max_cycle:
+                max_cycle = period
+                denom = d
+
+print(denom,max_cycle)
