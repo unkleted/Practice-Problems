464a352daccf61a8890a81b85836ec0a39ed505a
Justin Davis <jdavis@monkeywhiskers.xyz>
2021-09-08 19:32:04 -0400

    Added prime generator

--- math_stuff.py
+++ math_stuff.py
@@ -25,6 +25,38 @@ def prime_factors(number):
 
     return my_list
 
+
+def gen_primes():
+    """ Generate and infinite sequence of prime numbers."""
+    # Sieve of Eratosthenes
+    # Code by David Eppstean, UC Irvine, 28 Feb 2002
+    # http://code.activestate.com/recipes/117119/
+
+    # Maps composites to primes witnessing their compositeness. This is memory 
+    # efficient, as the siev is not "run forward" indefinitely, but only as long 
+    # as required by the curent number being tested
+
+    D = {}
+    # The running integer that's checked for primeness
+    q = 2
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't already marked in
+            # previous iterations
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that divide it. Since 
+            # we've reached q, we no longer need it in the map, but we'll mark
+            # the next multiples of its witnesses to prepare for larger numbers
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+        
+
 def slow_all_divisors(number):
     """Returns a list of all natural divisors of a number."""
     my_list = []
